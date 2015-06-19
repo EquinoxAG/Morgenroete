7d249f9fc6196dc1c0ba596126c3ba08ffe56518
Alexander Leonhardt <equinox.malexander@outlook.de>
2015-06-19 17:10:47 +0200

    Some bugfixing

--- test.asm
+++ test.asm
@@ -13,19 +13,24 @@ interface seq, intY, MyStruc
 
 interface_constructor MyStruc, main
 interface_destructor MyStruc, destu
+interface_constructor seq, main
 interface_destructor seq, destu
 
 
 DEFINE_CALL main, 3
 DEFINE_CALL destu, 1
 
+
+
 destu:
 global main
 main:
 	CreateStack myStack
-	ReserveStackSpace Mokkla, seq
+	ReserveStackSpace Mokkla, seq, rax, rdx
+	ReserveStackSpace Lokla, seq,dword[Mokkla.intY.tod.Mongo], rdx
+	UpdateStackPtr
 
-	mov qword[ Mokkla.intY.tod.Mongo+100 ], rax
+	mov rax, Mokkla.intY.tod.Mongo
 
 	DestroyStack myStack
 
