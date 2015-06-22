620f25ef574a65b67e7f2289f6b0d1c92cbcb24e
Alexander Leonhardt <equinox.malexander@outlook.de>
2015-06-22 18:51:24 +0200

    Added support for casting

--- test.asm
+++ test.asm
@@ -9,7 +9,7 @@ interface_close
 interface_open point
 	interface x_coord, qword
 	interface y_coord, qword
-	interface z_coord, qword
+	interface z_coord, dword
 interface_close
 
 interface_open multiboot2
@@ -21,19 +21,26 @@ interface_open multiboot
 	interface mb2, multiboot2
 interface_close
 
+%define MB_ADDR 0x1000
+
+
 destu:
 global main
 main:
 	mov rax, multiboot.sig
 	mov rbx, multiboot.rev
 	mov rcx, multiboot2.rev
-	
+
+
 
 
 	ConnectHeapToInterface MyIntok, multiboot
 
-	mov_ts rax, qword[ MyIntok.mb2.point3D.z_coord + MyIntok.rev ]
+	mov_ts dword[ ((MB_ADDR->multiboot).mb2.point3D->multiboot).sig ], 1000
+	mov_ts dword[ rax + multiboot.mb2 + multiboot2.point3D + multiboot.rev ], 200
+	mov_ts dword[ (MyIntok->multiboot2).point3D.z_coord ], 0
 
+	mov_ts rax, (rcx->multiboot2).point3D
 	uninterface multiboot
 	mov rdx, multiboot2.sig
 	ret
